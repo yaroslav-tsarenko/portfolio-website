fdfdb761a5054783feef0f36ef6b450ea99db9f1
Yaroslav <yaroslav7v@gmail.com>
2024-04-17 18:27:33 +0300

    resolved issues

--- src/components/header-component/HeaderComponent.tsx
+++ src/components/header-component/HeaderComponent.tsx
@@ -5,27 +5,45 @@ import "./HeaderComponent.css";
 import {
     ReactComponent as PersonalLogo
 } from "../../assets/images/logo.svg";
-import {useSwipeable} from "react-swipeable";
 
-const HeaderComponent = () => {
+interface TouchPosition {
+    x: number;
+    y: number;
+}
+
+const HeaderComponent: React.FC = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [startTouch, setStartTouch] = useState<TouchPosition>({ x: 0, y: 0 });
 
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
     };
-    const handlers = useSwipeable({
-        onSwipedLeft: () =>  setSidebarOpen(!sidebarOpen),
-        onSwipedRight: () => setSidebarOpen(!sidebarOpen),
-        trackMouse: true
-    });
+
+    const handleTouchStart = (e: React.TouchEvent) => {
+        const touch = e.touches[0];
+        setStartTouch({ x: touch.clientX, y: touch.clientY });
+    };
+
+    const handleTouchEnd = (e: React.TouchEvent) => {
+        const touch = e.changedTouches[0];
+        const endTouch: TouchPosition = { x: touch.clientX, y: touch.clientY };
+
+        if (Math.abs(endTouch.y - startTouch.y) < 50) {
+            if (endTouch.x > startTouch.x + 50) {
+                setSidebarOpen(false);
+            } else if (endTouch.x < startTouch.x - 50) {
+                setSidebarOpen(true);
+            }
+        }
+    };
+
     return (
-        <header className="header-nav">
+        <header className="header-nav" onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
             <div className="logo">
                 <a href="#" onClick={(event) => {
                     event.preventDefault();
                     window.scrollTo({top: 0, behavior: 'smooth'});
                 }}><PersonalLogo/></a>
-
             </div>
             <div className="nav-container">
                 <a href="#" onClick={(event) => {
@@ -48,7 +66,7 @@ const HeaderComponent = () => {
             <div className="burger-button" onClick={toggleSidebar}>
                 <FontAwesomeIcon icon={faBars as any}/>
             </div>
-            <div className={`sidebar ${sidebarOpen ? 'open' : ''}`} {...handlers}>
+            <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
                 <FontAwesomeIcon className="fa-times-sibebar-button" icon={faTimes as any} onClick={toggleSidebar}/>
                 <a href="#" onClick={(event) => {
                     event.preventDefault();
